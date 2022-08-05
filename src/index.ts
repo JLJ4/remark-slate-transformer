584bd625f7d8671f1e54f54003914c22c7b32515
Jochen Linnemann <jl@jochenlinnemann.de>
2022-08-05 12:08:04 +0200

    Remove legacy support

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export { default as remarkToSlate } from './plugins/remark-to-slate.js';
 export type { Options as RemarkToSlateOptions } from './plugins/remark-to-slate.js';
-export { default as remarkToSlateLegacy } from './plugins/remark-to-slate0.47.js';
 export { default as slateToRemark } from './plugins/slate-to-remark.js';
 export type { Options as SlateToRemarkOptions } from './plugins/slate-to-remark.js';
-export { default as slateToRemarkLegacy } from './plugins/slate0.47-to-remark.js';
 export { mdastToSlate } from './transformers/mdast-to-slate/index.js';
 export { slateToMdast } from './transformers/slate-to-mdast/index.js';
